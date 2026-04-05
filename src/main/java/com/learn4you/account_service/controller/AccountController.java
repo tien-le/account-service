4085a1b04552accad39de5d33f1c93b2e3716252
Tien LE <letien.dev@gmail.com>
2026-04-05 22:30:11 +0200

    LAVIE-210: Added Fallback for handleFailure, handleSuccess when sending kafkaTemplate

--- src/main/java/com/learn4you/account_service/controller/AccountController.java
+++ src/main/java/com/learn4you/account_service/controller/AccountController.java
@@ -4,7 +4,9 @@ import com.learn4you.account_service.model.AccountDTO;
 import com.learn4you.account_service.model.MessageDTO;
 import com.learn4you.account_service.model.StatisticDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaProducerException;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,9 +34,44 @@ public class AccountController {
                                           .content("Learn4You has been created for Continuous Learning Platform...")
                                           .build();
 
-        kafkaTemplate.send("notification", messageDTO);
+        /* Kafka errors:
+        SerializationException → serializer issue
+        TimeoutException → broker unreachable
+        UnknownTopicOrPartition → topic missing
+        NotLeaderForPartition → cluster issue
+         */
+        for (int i=0; i<1000; i++)
+            kafkaTemplate.send("notification", messageDTO).whenComplete((status, ex) -> {
+                if (ex != null) {
+                    // Fallback Logic
+                    // Handle failures, save to DB the failed events.
+                    // Then, using Job Scheduler to send these failed events again
+                    System.out.println("Error sending notification");
+//                    ex.printStackTrace();
+                    handleFailure(messageDTO, ex);
+                } else {
+                    // Handle success
+                    handleSuccess(status);
+                }
+            });
         kafkaTemplate.send("statistic", stat);
-
         return account;
     }
+
+    private void handleSuccess(SendResult<String, Object> status) {
+        System.out.println("Sent successfully : "  + status.getRecordMetadata().offset());
+    }
+
+    private void handleFailure(Object message, Throwable throwable) {
+        System.err.println("Error sending notification" + throwable.getMessage());
+
+        // Fallback Strategies
+//        saveToDatabase(message);
+//        sendToDeadLetterTopic(message);
+//        retrySend(message);
+
+        throwable.printStackTrace();
+    }
+
+
 }
